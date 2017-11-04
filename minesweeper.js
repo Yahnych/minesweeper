22535e6485b3ece40f05dca95466c1fe94f287ea
Sergey Chibiryaev <macmini@Betelgeuse.local>
2017-11-04 18:20:55 +0300

    Added skin and tiles

--- minesweeper.js
+++ minesweeper.js
@@ -3,6 +3,20 @@
 
 class Minesweeper {
     constructor(context) {
+        this.context = context;
 
+        function loadImage(path, callback) {
+            const image = new Image();
+            image.onload = () => {
+                callback(image);
+            };
+            image.onerror = () => {
+                alert("Could't loading image '" + path + "'.");
+            };
+            image.src = path;
+        }
+
+        loadImage("tiles.bmp", (image) => this.img_tiles = image);
+        loadImage("skin.bmp", (image) => this.img_skin = image);
     }
 }
