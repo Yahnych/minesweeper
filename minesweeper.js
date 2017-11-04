e24e66bf02446b9827b53de225251478c7276fd9
Sergey Chibiryaev <macmini@Betelgeuse.local>
2017-11-04 19:54:48 +0300

    Scalling

--- minesweeper.js
+++ minesweeper.js
@@ -4,6 +4,7 @@
 class Minesweeper {
     constructor(context) {
         this.context = context;
+        this.setScale(1);
 
         function loadImage(path) {
             return new Promise((resolve, reject) => {
@@ -29,6 +30,10 @@ class Minesweeper {
             .catch((name) => alert("Couldn't load image '" + name + "'"));
     }
 
+    setScale(scale) {
+        this.scale = scale;
+    }
+
     newGame(width, height, mines) {
         const clamp = (value, min, max) => Math.min(Math.max(value, min), max);
 
@@ -39,15 +44,19 @@ class Minesweeper {
     }
 
     draw() {
-        this.context.drawImage(this.img_skin, 0, 0, 12, 55, 0, 0, 12, 55);
-        this.context.drawImage(this.img_skin, 40, 0, 8, 55, this.width * 16 + 12, 0, 8, 55);
-        this.context.drawImage(this.img_skin, 12, 0, 20, 55, 12, 0, this.width * 16, 55);
-        this.context.drawImage(this.img_skin, 0, 72, 12, 8, 0, 55 + this.height * 16, 12, 8);
-        this.context.drawImage(this.img_skin, 20, 72, 8, 8, this.width * 16 + 12, 55 + this.height * 16, 8, 8);
-        this.context.drawImage(this.img_skin, 0, 56, 12, 10, 0, 55, 12, this.height * 16);
-        this.context.drawImage(this.img_skin, 12, 72, 8, 8, 12, 55 + this.height * 16, this.width * 16, 8);
-        this.context.drawImage(this.img_skin, 20, 64, 8, 8, this.width * 16 + 12, 55, 8, this.height * 16);
-        this.context.drawImage(this.img_skin, 48, 0, 41, 25, 16, 16, 41, 25);
-        this.context.drawImage(this.img_skin, 48, 0, 41, 25, 12 + this.width * 16 - 4 - 41, 16, 41, 25);
+        const draw = (sx, sy, swidth, sheight, dx, dy, dwidth, dheight) =>
+            this.context.drawImage(this.img_skin, sx, sy, swidth, sheight,
+                dx * this.scale, dy * this.scale, dwidth * this.scale, dheight * this.scale);
+
+        draw(0, 0, 12, 55, 0, 0, 12, 55);
+        draw(40, 0, 8, 55, this.width * 16 + 12, 0, 8, 55);
+        draw(12, 0, 20, 55, 12, 0, this.width * 16, 55);
+        draw(0, 72, 12, 8, 0, 55 + this.height * 16, 12, 8);
+        draw(20, 72, 8, 8, this.width * 16 + 12, 55 + this.height * 16, 8, 8);
+        draw(0, 56, 12, 10, 0, 55, 12, this.height * 16);
+        draw(12, 72, 8, 8, 12, 55 + this.height * 16, this.width * 16, 8);
+        draw(20, 64, 8, 8, this.width * 16 + 12, 55, 8, this.height * 16);
+        draw(48, 0, 41, 25, 16, 16, 41, 25);
+        draw(48, 0, 41, 25, 12 + this.width * 16 - 4 - 41, 16, 41, 25);
     }
 }
